27b6ca11979fdf93ff894b4d4d85bd020ed235b5
Justin Shaw <wyojustin@gmail.com>
2022-08-07 12:10:30 -0400

    made camera orientation and position solver more robust

--- grid_perspective_3.py
+++ grid_perspective_3.py
@@ -1,3 +1,4 @@
+from scipy.optimize import fmin, fmin_powell, minimize
 import PIL
 import pylab as pl
 import numpy as np
@@ -54,22 +55,67 @@ fig, ax = pl.subplots(1, figsize=(W/200, H/200))
 photo = np.array(PIL.Image.open('img/OpeningPosition.jpeg'))
 ax.imshow(photo, extent=(-W/2, W/2, -H/2, H/2), origin='upper')
 
-X, Y, Z = 200, 1000, 8500
-X, Y, Z = 0, 0, 10000
+positions = {'a1':np.array((-636, -374)),
+             'a8':np.array([-376.38139061,320.14850568]),
+             'h8':np.array([311.20660502, 317.08544763]),
+             'h1':np.array([532, -363]),
+             'a6': np.array([-426.45396426,188.16996774]),
+             'h6':np.array([354.62282089, 185.72452119]),
+             'h7':np.array([331.53872035, 255.56821376]),
+             'd1':np.array([-132, -374]),
+             'b7':np.array([-294.6754694,257.97434045]),
+             'e5':np.array([23, 106])
+}
+
+
+X, Y, Z = -300, 500, 8200
+X, Y, Z = 0, 500, 8200
 camera_position = [X, Y, Z]
 objects = []
 raw_squares = []
-for i in np.arange(-3.5, 5, 1):
-    for j in np.arange(-3.5, 5, 1):
-        if (i + j) % 2 == 0:
-            r = np.array([i * SIDE, j * SIDE, 0])
-            print(i, j, r)
-            raw_squares.append(r)
-            
-            s = (square + r)
-            p = project(s, FocalLength,
-                        X, Y, Z, roll, pitch, yaw)
-            objects.append(ax.fill(p[:,0], p[:,1], alpha=.8, color='r')[0])
+offset_x = 3.5 * SIDE
+offset_y = 3.5 * SIDE
+
+
+def get_row_col(alg):
+    row = ord(alg[0]) - ord('a')
+    col = int(alg[1]) - 1
+    return row, col
+
+def get_color(alg):
+    row, col = get_row_col(alg)
+    return (row + col) % 2
+
+def get_center(alg):
+    row, col = get_row_col(alg)
+    return np.array([row * SIDE - offset_x, col * SIDE - offset_y, 0])
+
+#for i in np.arange(-3.5, 4.5, 1):
+#    for j in np.arange(-3.5, 4.5, 1):
+for row in 'abcdefgh':
+    for col in range(1, 9):
+        alg = f'{row}{col}'
+        color = get_color(alg)
+        r = get_center(alg)
+        raw_squares.append(r)
+        s = (square + r)
+        p = project(s, FocalLength,
+                    X, Y, Z, roll, pitch, yaw)
+        if alg == 'a1':
+            objects.append((ax.fill(p[:,0], p[:,1], alpha=.3, color='k')[0], color))
+        elif color:
+            objects.append((ax.fill(p[:,0], p[:,1], alpha=.3, color='r')[0], color))
+        else:
+            objects.append((ax.fill(p[:,0], p[:,1], alpha=.3, color='w')[0], color))
+
+for row in 'ah':
+    for col in [1, 8]:
+        alg = f'{row}{col}'
+        r = get_center(alg)
+        p = project(r[np.newaxis], FocalLength,
+                    X, Y, Z, roll, pitch, yaw)[0]
+        #pl.plot(p[0], p[1], 'wd')
+       
 
 def getSquarePos3D(alg):
     x
@@ -85,7 +131,7 @@ def on_update(*args, **kw):
                     [0, 0, 1]])
 
     orient = Yaw @ Pitch @ Roll
-    for r, o in zip(raw_squares, objects):
+    for r, (o, color) in zip(raw_squares, objects):
         s = square + r
         p = project(s, FocalLength, X, Y, Z, roll, pitch, yaw)
         o.set_xy(p)
@@ -94,9 +140,54 @@ def on_update(*args, **kw):
         
 pl.axis('equal')
 
-def onclick(event):
-    global roll, pitch, yaw, Z
+methods = ['Nelder-Mead',
+           'Powell',    
+           'CG', 
+           'BFGS',    
+           'L-BFGS-B', 
+           'TNC',      
+           'COBYLA',   
+           'SLSQP',    
+           ]
 
+def onclick(event):
+    global FocalLength, X, Y, Z, roll, pitch, yaw
+    
+    def minme(params, args, plot=False):
+        print(params)
+        if len(params) > 0:
+            FocalLength = params[0]
+        if len(params) > 3:
+            X, Y, Z = params[1:4]
+        if len(params) > 6:
+            roll, pitch, yaw = params[4:7]
+        else:
+            roll, pitch, yaw = args
+        err = 0
+        for alg in positions:
+            pixel_center = positions[alg]
+            center = get_center(alg)
+            projected_center = project(center[np.newaxis], FocalLength, X, Y, Z, roll, pitch, yaw)
+            err += np.linalg.norm(projected_center - pixel_center)
+            #err += prior_w @ (params - prior) ** 2
+            if plot:
+                print(alg, pixel_center, projected_center)
+                pl.plot(projected_center[0,0], projected_center[0,1], 'gd')
+                pl.plot(pixel_center[0], pixel_center[1], 'rd')
+        return err
+
+    for method in methods:
+        guess= [FocalLength, X, Y, Z, roll, pitch, yaw ]
+        guess = [FocalLength, X, Y, Z]
+        args = [roll, pitch, yaw]
+        ans = minimize(minme, guess ,method=method, args=args)['x']
+        print(method, minme(ans, args, plot=True))
+        fig.canvas.draw_idle()
+        print(ans)
+        break
+    on_update()
+    fig.canvas.draw_idle()
+    return
     if False:
         print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
               ('double' if event.dblclick else 'single', event.button,
@@ -119,10 +210,10 @@ def onclick(event):
         Z += 100 * dir
         camera_position[2] = Z
     print(np.round(roll / DEG), np.round(pitch / DEG), np.round(yaw / DEG), Z)
-    on_update()
-    fig.canvas.draw_idle()
+
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
 pl.show()
 ### fit params
 # FocalLength, X, Y, Z, roll, pitch, yaw
+
---
+++ grid_perspective_4.py
@@ -0,0 +1,220 @@
+from scipy.optimize import fmin, fmin_powell, minimize
+import PIL
+import pylab as pl
+import numpy as np
+from numpy import cos, sin
+
+DEG = np.pi / 180
+
+def get_row_col(alg):
+    row = ord(alg[0]) - ord('a')
+    col = int(alg[1]) - 1
+    return row, col
+
+def get_color(alg, dark='r', light='w'):
+    row, col = get_row_col(alg)
+    is_dark = (row + col + 1) % 2
+    if alg == 'a1':
+        color = 'k'
+    elif is_dark:
+        color = dark
+    else:
+        color = light
+    return color
+    
+
+def get_center(alg, side):
+    offset_x = 3.5 * side ## x offset to center of the board
+    offset_y = 3.5 * side ## y offset to center of the board
+    row, col = get_row_col(alg)
+    return np.array([row * side - offset_x, col * side - offset_y, 0])
+
+def getOrient(roll, pitch, yaw):
+    Roll = np.array([[1, 0, 0],
+                     [0, cos(roll), -sin(roll)],
+                     [0, sin(roll),  cos(roll)]])
+    Pitch = np.array([[cos(pitch), 0, -sin(pitch)],
+                      [0, 1, 0],
+                      [sin(pitch), 0,  cos(pitch)]])
+    Yaw = np.array([[cos(yaw), -sin(yaw), 0],
+                    [sin(yaw),  cos(yaw), 0],
+                    [0, 0, 1]])
+
+    orient = Yaw @ Pitch @ Roll
+    return orient
+
+def project(points, FocalLength,
+            X, Y, Z, roll, pitch, yaw):
+    orient = getOrient(roll, pitch, yaw)
+    pos = np.array([X, Y, Z])
+    doa = points - pos
+    s = (orient @ doa.T).T
+
+    # scale to make focal lenght in third column
+    doa = FocalLength * s / s[:,2,np.newaxis]
+    return doa[:,:2]
+
+def invert(points2d, FocalLength,
+           X, Y, Z, roll, pitch, yaw):
+    orient = getOrient(roll, pitch, yaw)
+    pos = np.array([X, Y, Z])
+    n = len(points2d)
+    doa = np.hstack([points2d, np.ones(n)[:,np.newaxis] * FocalLength])
+    s = (orient.T @ doa.T).T
+
+    doa = (orient.T @ doa.T).T + pos
+    ## [x, y, 0] = pos + doa3 * l
+    ## pos[2] + doa3[:,2] * l = 0
+    ## l = -pos[2] / doa3[:,2]
+    l = pos[2] / doa[:,2]
+    points = doa * l[:,np.newaxis]
+    return points
+
+class Square:
+    def __init__(self, side, alg, dark, light):
+        self.alg = alg
+        self.pos = get_center(alg, side)
+        self.square = np.column_stack([[-1, -1, 1, 1],
+                                       [-1, 1, 1, -1],
+                                       [0, 0, 0, 0]]) * side / 2 + self.pos
+        self.color = get_color(alg, dark, light)
+        self.side = side
+
+class ChessBoard:
+    def __init__(self, side, dark, light):
+        self.squares = []
+        for row in 'abcdefgh':
+            for col in range(1, 9):
+                alg = f'{row}{col}'
+                self.squares.append(Square(side, alg, dark, light))
+
+class Camera:
+    def __init__(self, focal_length, px, py, pz, ar, ap, ay):
+        self.focal_length = focal_length
+        self.pos = np.array([px, py, pz])
+        self.rpy = np.array([ar, ap, ay])
+        self.orient = getOrient(ar, ap, ay)
+        #self.fig, self.ax = pl.subplots(1)
+        #cid = self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+        self.objects = []
+        
+    def project(self, points):
+        return project(points, self.focal_length, *self.pos, *self.rpy)
+    
+    def fill(self, points, ax, *args, **kw):
+        p2d = self.project(points)
+        self.objects.append((points, ax.fill(p2d[:,0], p2d[:,1], *args, **kw)[0]))
+
+    def plot(self, points, ax, *args, **kw):
+        p2d = self.project(points)
+        self.objects.append((points, ax.plot(p2d[:,0], p2d[:,1], *args, **kw)[0]))
+        
+    def snapshot(self, board, ax):
+        for square in board.squares:
+            self.fill(square.square, ax, color=square.color, alpha=.3)
+            #self.plot(square.pos[np.newaxis], ax, 'k+')
+
+    def get_pixel_locs(self, board):
+        out = {}
+        for square in board.squares:
+            p2d = self.project(square.pos[np.newaxis])[0]
+            out[square.alg] = p2d
+        return out
+    
+    def on_click(self, event):
+        print(event)
+
+def get_error(board, cam1, cam2):
+    slack = 1
+    p1 = cam1.get_pixel_locs(board)
+    p2 = cam2.get_pixel_locs(board)
+    out = 0
+    for key in p1:
+        delta = np.linalg.norm(p1[key] - p2[key])
+        if delta > slack:
+            out += delta
+    return out
+    
+side = 1000
+board = ChessBoard(side, 'r', 'w')
+other_board = ChessBoard(side, 'g', 'w')
+
+focal_length = 1000
+px, py, pz = 0, 0, 8200
+ar = 0 * DEG
+ap = 0 * DEG
+ay = 180 * DEG
+
+xx = [[100, 2, 0], [4, 5, 0]]
+p = project(np.array(xx), focal_length, px, py, pz, ar, ap, ay)
+i = invert(p, focal_length, px, py, pz, ar, ap, ay)
+
+print(xx)
+print(i)
+
+meas_camera = Camera(focal_length, px, py, pz, ar, ap, ay)
+test_camera = Camera(focal_length, 0, -500, 5000, 20 * DEG, 5 * DEG, 5 * DEG)
+
+### read true positions:
+with open('true_positions.txt') as f:
+    lines = f.readlines()
+data = [l.split() for l in lines]
+data = [[l[0], float(l[1]), float(l[2])] for l in data]
+dict = {}
+for l in data:
+    dict[l[0]] = l[1:]
+
+
+def minme(xyz):
+    test_camera = Camera(focal_length, xyz[0], xyz[1], xyz[2], ar, ap, ay)
+    return get_error(board, meas_camera, test_camera)
+
+def angle_pen(a):
+    if -np.pi < a and a < np.pi:
+        out = 0
+    else:
+        out = np.abs(a) - np.pi
+    return out
+
+def minmer(xyzr):
+    test_camera = Camera(focal_length, xyzr[0], xyzr[1], xyzr[2], xyzr[3], ap, ay)
+    out = get_error(board, meas_camera, test_camera)
+    out += angle_pen(xyzr[3])
+    return out
+
+def minmerp(xyzrp):
+    test_camera = Camera(focal_length, xyzrp[0], xyzrp[1], xyzrp[2], xyzrp[3], xyzrp[4], ay)
+    out = get_error(board, meas_camera, test_camera)
+    out += angle_pen(xyzrp[3])
+    out += angle_pen(xyzrp[4])
+    return out
+
+def minmerpy(xyzrpy):
+    test_camera = Camera(focal_length, xyzrpy[0], xyzrpy[1], xyzrpy[2], xyzrpy[3], xyzrpy[4], xyzrpy[5])
+    out = get_error(board, meas_camera, test_camera)
+    out += angle_pen(xyzrpy[3])
+    out += angle_pen(xyzrpy[4])
+    out += angle_pen(xyzrpy[5])
+    return out
+
+if False:
+    ans = fmin_powell(minme, [0, 0, 8000])
+    print(ans)
+    ans = fmin_powell(minmer, [0, 0, 8000, 20 * DEG])
+    print(ans)
+    ans = fmin_powell(minmerp, [0, 0, 8000, ar, ap])
+    print(ans)
+guess = [-100, -300, 5000, 0 * DEG, 0 * DEG, 0 * DEG]
+guss_camera = Camera(focal_length, *guess)
+#ans = fmin_powell(minmerpy, guess)
+#print(ans)
+#soln_camera = Camera(focal_length, *ans)
+
+fig, ax = pl.subplots(1)
+meas_camera.snapshot(board, ax)
+#soln_camera.snapshot(board, ax)
+guss_camera.snapshot(other_board, ax)
+#pl.axes('equal')
+pl.show()
+
+here
