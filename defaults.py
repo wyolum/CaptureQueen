7cc147f650446c576c41b418d9bf5ad8e44a0376
Justin Shaw <wyojustin@gmail.com>
2022-08-27 04:58:39 -0400

    defaults

---
+++ defaults.py
@@ -0,0 +1,9 @@
+dark_green = '#aaaaaa'
+dark_green = '#66aa66'
+dark_red = '#aa3333'
+colors = {'square dark':dark_red,
+          'square light':"#bbbbbb",
+          'square light lastmove':'#ffaaaa',
+          'square dark lastmove':"#bb6666",
+          'margin':'#cccccc',
+          'coord':dark_red}
